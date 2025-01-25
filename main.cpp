a48a11061ab74f3823d63e7fba63f0689124b361
abeckley27 <aidanbeckley@gmail.com>
2025-01-25 18:11:57 -0500

    Added trials at different sizes

--- main.cpp
+++ main.cpp
@@ -5,16 +5,11 @@
 #include <cstdint>
 #include <omp.h>
 
-
-
-int main(int argc, char* argv[]) {
-
-	int64_t N = 1000;
-	if (argc > 1) N = std::stoi(std::string(argv[1]));
+double run(int64_t N) {
 	int i, j, k;
 
 	// Initialize Matrices
-	float **A, **B, **C;
+	float** A, ** B, ** C;
 	A = new float* [N];
 	B = new float* [N];
 	C = new float* [N];
@@ -48,11 +43,31 @@ int main(int argc, char* argv[]) {
 
 	double t1 = omp_get_wtime();
 
-	int64_t flops = N * N * N;
-	std::cout << "Time: " << (t1 - t0) << std::endl;
-	std::cout << flops / (1000000000 * (t1 - t0)) << "Gflops\n";
+	for (i = 0; i < N; i++) {
+		delete[] A[i];
+		delete[] B[i];
+		delete[] C[i];
+	}
+
+	return t1 - t0;
+}
+
+int main(int argc, char* argv[]) {
+
+	int64_t N = 2000;
+	if (argc > 1) N = std::stoi(std::string(argv[1]));
+
+	for (int64_t i = 1; i < N/100; i += 1) {
+		double dt = run(i * 100);
+		int64_t flops = i * i * i * 1e6;
+		std::cout << "Size: " << i * 100 << std::endl;
+		std::cout << "Time: " << dt << std::endl;
+		std::cout << flops / (1000000000 * dt) << "Gflops\n";
+		std::cout << "----------\n";
+	}
 
 
+/*
 	std::ofstream f;
 	f.open("Matrix_output.txt");
 	for (i = 0; i < N; i++) {
@@ -62,7 +77,7 @@ int main(int argc, char* argv[]) {
 		f << std::endl;
 	}
 	f.close();
-
+*/
 
 
 
